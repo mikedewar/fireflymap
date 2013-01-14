dd656ce7a7f6f86cbeff395e4cdca622b1e995fe
Mike Dewar <mikedewar@gmail.com>
2013-01-14 07:40:55 -0500

    tidied up node code

--- server.js
+++ server.js
@@ -1,52 +1,42 @@
 var http = require('http'),
     io = require('socket.io'),
-    fs = require('fs');
-
-var express = require('express');
-var app = express();
-var server = http.createServer(app);
+    fs = require('fs'),
+    express = require('express');
 
 // serve files
+var app = express();
+var server = http.createServer(app);
 app.use(express.static(__dirname + '/'));
-
 server.listen(8000)
 
 // create the stream -> websockets connection
-
 options = {
     hostname: 'developer.usa.gov',
     path: '/1usagov',
 }
 
-function callback(res){
-    res.on('data', function(chunk){
-        console.log(chunk.toString())
-    })
-}
-
 var listener = io.listen(server)
 
 listener.sockets.on('connection', function (socket) {
-
+    // once the socket connection is open, make the http request to the stream
     var req = http.request(options, function(res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
         res.setEncoding('utf8');
         res.on('data', function (chunk) {
-        try {
-            click = JSON.parse(chunk)
-            if (click.ll) {
-                socket.emit('click', click)
-    //            console.log(click.ll)
+            // when we get data (chunk) try to parse it
+            try {
+                click = JSON.parse(chunk)
+                if (click.ll) {
+                    // if the parsed JSON has a lat/lon in it, then send it to the client
+                    socket.emit('click', click)
+                }
+            } catch (err) {
+                // print out parsing errors
+                console.log("ERR: " + err)
             }
-        } catch (err) {
-            console.log(chunk)
-            console.log("ERR: " + err)
-        }
       });
     });
+    // (oddly?) we call the request's .end() method to get the whole thing going
     req.end()
-    
 });
 
 
